b2a31ab90786af346775f34e6fd2120ebe8b56d0
mo49 <k.mochizuki.0409@gmail.com>
2018-10-12 16:11:47 +0900

    add cssnano

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,14 @@
 module.exports = {
     plugins: [
-        require('autoprefixer')(),
         require('postcss-assets')({
             basePath: `public/`,
             loadPaths: [ `img/` ],
         }),
+        require('autoprefixer')({
+            browsers: ['last 2 versions']
+        }),
+        require('cssnano')({
+            autoprefixer: false
+        })
     ]
 };
