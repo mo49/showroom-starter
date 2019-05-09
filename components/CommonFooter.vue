6174314efa95799714e2f96c9b982b9eb73eb6e7
mo49 <k.mochizuki.0409@gmail.com>
2019-05-09 15:25:51 +0900

    add common components

---
+++ components/CommonFooter.vue
@@ -0,0 +1,9 @@
+<template>
+  <footer class="footer">
+    <small>&copy; Kazuki Mochizuki</small>
+  </footer>
+</template>
+
+<style lang="scss">
+// @import "~assets/sass/module/footer";
+</style>
---
+++ components/CommonHeader.vue
@@ -0,0 +1,11 @@
+<template>
+  <header class="header">
+    <nuxt-link :to="{name: 'index'}">
+      <h1 class="header__logo">logo</h1>
+    </nuxt-link>
+  </header>
+</template>
+
+<style lang="scss">
+// @import "~assets/sass/module/header";
+</style>
