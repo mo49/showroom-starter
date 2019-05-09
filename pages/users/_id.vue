15f0d6fe8641e95a89ff4826eff17984452367cd
mo49 <k.mochizuki.0409@gmail.com>
2019-05-09 15:07:02 +0900

    ユーザーページを動的に生成

---
+++ pages/users/_id.vue
@@ -0,0 +1,15 @@
+<template>
+  <section class="container">
+    <h1>welcome, user {{ $route.params.id }}</h1>
+  </section>
+</template>
+
+<script>
+export default {
+  components: {
+  },
+  validate({params}) {
+    return /^\d+$/.test(params.id)
+  }
+}
+</script>
